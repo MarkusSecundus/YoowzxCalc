edce680fa20a47f34e44c49a7d2097b7f2b60cc7
MarkusSecundus <62842675+MarkusSecundus@users.noreply.github.com>
2021-08-06 09:57:39 +0200

    added a structure for describing function signatures

---
+++ MarkusSecundus.ProgrammableCalculator/Parser/FunctionSignature.cs
@@ -0,0 +1,25 @@
+﻿using MarkusSecundus.ProgrammableCalculator.Numerics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkusSecundus.ProgrammableCalculator.Parser
+{
+    public struct FunctionSignature<TNumber> : IEquatable<FunctionSignature<TNumber>> where TNumber : INumber<TNumber>
+    {
+        public string Name { get; init; }
+        public int ArgsCount { get; init; }
+
+
+        public bool Equals(FunctionSignature<TNumber> s) => (Name, ArgsCount) == (s.Name, s.ArgsCount);
+
+        public override bool Equals(object obj) => obj is FunctionSignature<TNumber> s && Equals(s);
+
+
+        public override int GetHashCode() => (Name, ArgsCount).GetHashCode();
+
+        public override string ToString() => $"{Name}<{typeof(TNumber).Name}>({ArgsCount})";
+    }
+}
