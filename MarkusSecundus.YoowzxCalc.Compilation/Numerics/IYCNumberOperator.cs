08abbfeb15956e7215efe31cea0a19a38d28278e
MarkusSecundus <62842675+MarkusSecundus@users.noreply.github.com>
2021-08-31 23:02:50 +0200

    added some doccomments

--- MarkusSecundus.YoowzxCalc.Compilation/Numerics/IYCNumberOperator.cs
+++ MarkusSecundus.YoowzxCalc.Compilation/Numerics/IYCNumberOperator.cs
@@ -6,26 +6,82 @@ using System.Collections.Generic;
 namespace MarkusSecundus.YoowzxCalc.Numerics
 {
 
+    //TODO: finish documentation!
+    /// <summary>
+    /// Object providing operations that can be performed on the specified number type by the calculator.
+    /// </summary>
+    /// <typeparam name="TNumber">Numeric type used to carry computational data.</typeparam>
     public interface IYCNumberOperator<TNumber>
     {
+        /// <summary>
+        /// Tries to convert provided string to its numeric value.
+        /// </summary>
+        /// <param name="repr">Text representation of a number.</param>
+        /// <param name="value">Result value. Will be set to the respective value if the conversion succeeds or to <c>default(TNumber)</c> if it failed.</param>
+        /// <returns>Whether the conversion was successfull - aka. the string is a valid constant representator.</returns>
         public bool TryParse(string repr, out TNumber value);
 
+        /// <summary>
+        /// Determines whether the string represents a valid identifier in the calculator grammar.
+        /// </summary>
+        /// <param name="identifier">Identifier candidate to be validated.</param>
+        /// <returns>Whether the string is a valid identifier</returns>
         public FormatException ValidateIdentifier(string identifier);
 
-
+        /// <summary>
+        /// Collection of functions to be available for calling from calculator by default.
+        /// </summary>
         public IReadOnlyDictionary<YCFunctionSignature<TNumber>, Delegate> StandardLibrary
             => CollectionsUtils.EmptyDictionary<YCFunctionSignature<TNumber>, Delegate>();
 
+        /// <summary>
+        /// Add two numbers (<c>'x+y'</c> in C-like languages)
+        /// </summary>
+        /// <param name="a">First operand</param>
+        /// <param name="b">Second operand</param>
+        /// <returns>Result of the addition</returns>
         public TNumber Add(TNumber a, TNumber b);
 
+        /// <summary>
+        /// Subtract one number from other (<c>'x-y'</c> in C-like languages)
+        /// </summary>
+        /// <param name="a">First operand</param>
+        /// <param name="b">Second operand</param>
+        /// <returns>Result of the subtraction</returns>
         public TNumber Subtract(TNumber a, TNumber b);
 
+        /// <summary>
+        /// Multiply two numbers (<c>'x*y'</c> in C-like languages)
+        /// </summary>
+        /// <param name="a">First operand</param>
+        /// <param name="b">Second operand</param>
+        /// <returns>Product of the two numbers</returns>
         public TNumber Multiply(TNumber a, TNumber b);
 
+
+        /// <summary>
+        /// Divide a number by another (<c>'x/y'</c> in C-like languages)
+        /// </summary>
+        /// <param name="a">First operand</param>
+        /// <param name="b">Second operand</param>
+        /// <returns>Result of the division</returns
         public TNumber Divide(TNumber a, TNumber b);
 
+
+        /// <summary>
+        /// Compute a division remainder (<c>'x%y'</c> in C-like languages)
+        /// </summary>
+        /// <param name="a">First operand</param>
+        /// <param name="b">Second operand</param>
+        /// <returns>Result of the modulo</returns
         public TNumber Modulo(TNumber a, TNumber b);
 
+
+        /// <summary>
+        /// Negate a number arithmetically (<c>'-x'</c> in C-like languages)
+        /// </summary>
+        /// <param name="a">The operand</param>
+        /// <returns>Result of the negation</returns
         public TNumber UnaryMinus(TNumber a);
 
         public TNumber Power(TNumber a, TNumber power);
--- MarkusSecundus.YoowzxCalc.DSL.AST/YCVisitorExtensions.cs
+++ MarkusSecundus.YoowzxCalc.DSL.AST/YCVisitorExtensions.cs
@@ -3,17 +3,47 @@ using MarkusSecundus.YoowzxCalc.DSL.AST.OtherExpressions;
 using MarkusSecundus.YoowzxCalc.DSL.AST.PrimaryExpression;
 using MarkusSecundus.YoowzxCalc.DSL.AST.UnaryExpressions;
 
-
 namespace MarkusSecundus.YoowzxCalc.DSL.AST
 {
+
+    /// <summary>
+    /// Static class containing extension functions to make working with YC visitors more convenient.
+    /// </summary>
     public static class YCVisitorExtensions
     {
+        /// <summary>
+        /// Accept a visitor.
+        /// </summary>
+        /// <typeparam name="TRet">Result type of the visit.</typeparam>
+        /// <typeparam name="TContext">Context type of the visit.</typeparam>
+        /// <param name="self">The expression node being visited.</param>
+        /// <param name="visitor">The visitor.</param>
+        /// <param name="ctx">Context for the visit.</param>
+        /// <returns>Result of the visit.</returns>
         public static void Accept<TRet, TContext>(this YCExpression self, YCVisitorBaseNoReturn<TRet, TContext> visitor, TContext ctx)
             => self.Accept(visitor, ctx);
 
+        /// <summary>
+        /// Accept a visitor.
+        /// </summary>
+        /// <typeparam name="TRet">Result type of the visit.</typeparam>
+        /// <typeparam name="TContext">Context type of the visit.</typeparam>
+        /// <param name="self">The expression node being visited.</param>
+        /// <param name="visitor">The visitor.</param>
+        /// <param name="ctx">Context for the visit.</param>
+        /// <returns>Result of the visit.</returns>
         public static TRet Accept<TRet, TContext>(this YCExpression self, YCVisitorBaseNoContext<TRet, TContext> visitor)
             => self.Accept(visitor, default);
 
+        /// <summary>
+        /// Accept a visitor.
+        /// </summary>
+        /// <typeparam name="TRet">Result type of the visit.</typeparam>
+        /// <typeparam name="TContext">Context type of the visit.</typeparam>
+        /// <param name="self">The expression node being visited.</param>
+        /// <param name="visitor">The visitor.</param>
+        /// <param name="ctx">Context for the visit.</param>
+        /// <returns>Result of the visit.</returns>
         public static void Accept<TRet, TContext>(this YCExpression self, YCVisitorBaseNoReturnNoContext<TRet, TContext> visitor)
             => self.Accept(visitor, default);
     }
