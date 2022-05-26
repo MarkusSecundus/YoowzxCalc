c2e4dc792a4b45b8cdbfd7004f4acd04dbe06af8
MarkusSecundus <kubahronik.g@gmail.com>
2022-05-26 15:10:57 +0200

    created some unittest projects

---
+++ MarkusSecundus.YoowzxCalc.DSL.Parser.UnitTests/LexerTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkusSecundus.YoowzxCalc.DSL.Parser.UnitTests
+{
+    internal class LexerTests
+    {
+        [Test]
+        public void SkipsWhitespace()
+        {
+        }
+    }
+}
