a5ad65896e9bdc23d810553beb4375b58bbb07a3
MarkusSecundus <kubahronik.g@gmail.com>
2022-05-27 00:17:43 +0200

    added few tests

--- MarkusSecundus.YoowzxCalc.DSL.AST.UnitTests/VisitorTests.cs
+++ MarkusSecundus.YoowzxCalc.DSL.AST.UnitTests/VisitorTests.cs
@@ -26,7 +26,7 @@ namespace MarkusSecundus.YoowzxCalc.DSL.AST.UnitTests
             AnyExpression
         }
 
-        public class TestVisitor : YCVisitorBase<VisitorStates, int>
+        public class TestVisitor1 : YCVisitorBase<VisitorStates, int>
         {
             public VisitorStates TestVariable = VisitorStates.None;
 
@@ -54,14 +54,10 @@ namespace MarkusSecundus.YoowzxCalc.DSL.AST.UnitTests
             public override VisitorStates Visit(YCConditionalExpression expr, int ctx) => TestVariable = VisitorStates.Conditional;
             public override VisitorStates Visit(YCFunctioncallExpression expr, int ctx) => TestVariable = VisitorStates.Functioncall;
             public override VisitorStates Visit(YCLiteralExpression expr, int ctx) => TestVariable = VisitorStates.Literal;
-
-
         }
 
-        [Test]
-        public void Visitor_DoesVisit()
+        private void test_doesPerformLeafActions(IYCVisitor<VisitorStates, int> v, Func<VisitorStates> stateGetter)
         {
-            TestVisitor v = new();
             YCExpression e;
 
             test<YCUnaryLogicalNotExpression>(VisitorStates.UnaryLogicalNot);
@@ -92,8 +88,59 @@ namespace MarkusSecundus.YoowzxCalc.DSL.AST.UnitTests
             {
                 e = new TExpr();
                 e.Accept(v, 0);
-                Assert.AreEqual(v.TestVariable, state);
+                Assert.AreEqual(stateGetter(), state);
             }
+        }
+
+
+        [Test]
+        public void Visitor_DoesVisit()
+        {
+            var v = new TestVisitor1();
+            test_doesPerformLeafActions(v, () => v.TestVariable);
+        }
+
+
+
+        public class TestVisitor2 : YCVisitorBase<VisitorStates, int>
+        {
+            public VisitorStates TestVariable = VisitorStates.None;
+
+
+            public override VisitorStates Visit(YCBinaryExpression expr, int ctx) => VisitorStates.AnyBinary;
+            public override VisitorStates Visit(YCUnaryExpression expr, int ctx) => VisitorStates.AnyUnary;
+            public override VisitorStates Visit(YCPrimaryExpression expr, int ctx) => VisitorStates.AnyPrimary;
+
+            public override VisitorStates Visit(YCUnaryLogicalNotExpression expr, int ctx) => TestVariable = VisitorStates.UnaryLogicalNot;
+            public override VisitorStates Visit(YCUnaryMinusExpression expr, int ctx) => TestVariable = VisitorStates.UnaryMinus;
+            public override VisitorStates Visit(YCUnaryPlusExpression expr, int ctx) => TestVariable = VisitorStates.UnaryPlus;
+
+            public override VisitorStates Visit(YCAddExpression expr, int ctx) => TestVariable = VisitorStates.Add;
+            public override VisitorStates Visit(YCSubtractExpression expr, int ctx) => TestVariable = VisitorStates.Subtract;
+            public override VisitorStates Visit(YCMultiplyExpression expr, int ctx) => TestVariable = VisitorStates.Multiply;
+            public override VisitorStates Visit(YCDivideExpression expr, int ctx) => TestVariable = VisitorStates.Divide;
+            public override VisitorStates Visit(YCModuloExpression expr, int ctx) => TestVariable = VisitorStates.Modulo;
+            public override VisitorStates Visit(YCExponentialExpression expr, int ctx) => TestVariable = VisitorStates.Exponential;
+
+            public override VisitorStates Visit(YCCompareGreaterOrEqualExpression expr, int ctx) => TestVariable = VisitorStates.GreaterOrEqual;
+            public override VisitorStates Visit(YCCompareGreaterThanExpression expr, int ctx) => TestVariable = VisitorStates.GreaterThan;
+            public override VisitorStates Visit(YCCompareLessOrEqualExpression expr, int ctx) => TestVariable = VisitorStates.LessOrEqual;
+            public override VisitorStates Visit(YCCompareLessThanExpression expr, int ctx) => TestVariable = VisitorStates.LessThan;
+            public override VisitorStates Visit(YCCompareIsEqualExpression expr, int ctx) => TestVariable = VisitorStates.IsEqual;
+            public override VisitorStates Visit(YCCompareIsNotEqualExpression expr, int ctx) => TestVariable = VisitorStates.IsNotEqual;
+
+            public override VisitorStates Visit(YCLogicalAndExpression expr, int ctx) => TestVariable = VisitorStates.LogicalAnd;
+            public override VisitorStates Visit(YCLogicalOrExpression expr, int ctx) => TestVariable = VisitorStates.LogicalOr;
+            public override VisitorStates Visit(YCConditionalExpression expr, int ctx) => TestVariable = VisitorStates.Conditional;
+            public override VisitorStates Visit(YCFunctioncallExpression expr, int ctx) => TestVariable = VisitorStates.Functioncall;
+            public override VisitorStates Visit(YCLiteralExpression expr, int ctx) => TestVariable = VisitorStates.Literal;
+        }
+
+        [Test]
+        public void Visitor_MoreConcreteOneOverridesGeneralOne()
+        {
+            var v = new TestVisitor2();
+            test_doesPerformLeafActions(v, () => v.TestVariable);
         }
     }
 }
