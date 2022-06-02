4e834e442db4b43c014303a83002305cb34aae5d
MarkusSecundus <kubahronik.g@gmail.com>
2022-06-02 22:13:55 +0200

    created tests project for Utils

---
+++ Tests/MarkusSecundus.Util.UnitTests/ActionOnDispose_Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkusSecundus.Util.UnitTests
+{
+    internal class ActionOnDispose_Tests
+    {
+    }
+}
